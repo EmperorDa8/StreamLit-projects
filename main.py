a60c6fc096b9e96d5fff4c334cc39004e984a947
Dan Usman <86113954+EmperorDa8@users.noreply.github.com>
2024-02-20 10:11:14 +0100

    Update main.py
    made some adjustments to the content

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ slt.set_page_config(page_title = 'Dan profile-website', page_icon=':waving_hand:
 
 with slt.container():
     slt.subheader('Hello i am Dan Usman :video_game:')
-    slt.title('i am an aspiring junior system administrator / junior cloud engineer currently located in Nigeria')
-    slt. write('i am a passionate individual looking to leverage my skillset to pursue a rewarding career in cloud computing')
+    slt.title('Experienced Junior System Administrator with a Strong Foundation in Google IT Automation and Prompt Engineering')
+    slt. write('i am a passionate individual looking to leverage my skillset to pursue a rewarding career in cloud computing and Gen AI)
     slt.write('[learn more through my Linkedin >](https://www.linkedin.com/in/dan-usman-b87282134/)')
 #---objectives
 def lottie_urlget(url):
@@ -38,7 +38,7 @@ with slt.container():
     with left_column:
         slt.header('my objectives ')
         slt.write('###')
-        slt.write('''Seeking a challenging and rewarding internship or entry-level opportunity to apply my Python skills and contribute to a dynamic cloud team. Open to learning new technologies and eager to make a positive impact.
+        slt.write('''Seeking a challenging and rewarding internship or entry-level opportunity to apply my IT/cloud skills and contribute to a dynamic cloud team. Open to learning new technologies and eager to make a positive impact.
         
         -Leverage Python scripting to automate routine system administration tasks such as user provisioning, configuration management, and data backup/recovery.
         -Applying automation solutions to scale infrastructure and operations in cloud environments.
@@ -77,7 +77,7 @@ Learnt how to interact with the operating system using Python modules like os an
 Automate tasks like managing users and groups, scheduling jobs, and monitoring system resources.
 
 5. Working with Databases:
-Understand the basics of relational databases and connect to them using Python libraries like pymysql and sqlalchemy.
+Understand the basics of relational databases and connect to them using Python libraries like MySQL and sqlalchemy.
 Write scripts to query databases, extract data, and perform CRUD operations.
  
            """)
@@ -93,18 +93,19 @@ with slt.container():
         slt.header('my recent certification > getting started with generative AI studio 2023')
         slt.write('###')
         slt.write('''
-        some hands on experience gained from this certification include the following
+        some hands-on experience gained from this certification include the following
         
 -solid understanding of generative AI concepts like text generation, translation, image creation, and code generation.
 -learnt about the core functionality of Generative AI Studio, including its user interface, pre-trained models, and tools for experimentation.
-other exploration include :
+other explorations include :
 
--experimenting Google latest AI model gemini for simple task like image extraction, image Q/A, image classificaton , and others with its pre-trained models for
+-experimenting with Google's latest AI model Gemini for simple tasks like image extraction, image Q/A, image classification, and others with its pre-trained models for
 prompt output
          
--also I have been able to leverage the platform to create AI chatbot , enterprize search app which can be deployed with API integration or widget integration to existing application      
+-also I have been able to leverage the platform to create an AI chatbot, and enterprise search app that can be deployed with API integration or widget integration to existing application      
         ''')
         slt.write('[learn more >](https://coursera.org/share/00162df6e1184066de5b34bf4f2d0b54)')
+        slt.write('[access the enterpise AI search app created and deployed by me using cloud run >](https://apptest-cdxrwirz7a-nw.a.run.app/)')
     with img_column:
         slt.image(GCP_gen)
 
@@ -131,7 +132,7 @@ with slt.container():
         slt.image(project_gen)
     with text_column:
         slt.subheader('My Generative AI projects attempts')
-        slt.write('this projects involves my attempts in using chatgpt Open API and also AI image tools like lenardo AI, and finally generative AI studio with prompt generating including latest multimodal model GEMINI')
+        slt.write('this projects involves my attempts in using chatgpt Open API and also AI image tools like lenardo AI, and finally generative AI studio with prompt engineering including latest multimodal model GEMINI-ultra')
         slt.write('[learn more >](https://github.com/EmperorDa8/generativeAI)')
         slt.write('Experience the power Ai semantic search through AI chatbot i created by Google vertex AI which i loaded with my dataset on cloud storage instance set up in background also develop AI enterprise search on the same platform ')
         slt.subheader('My AI Chatbot')
